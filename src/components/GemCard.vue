4a6771a2f2176f35308651b4e9e07b7983277c20
Illia <xmelnyk@mendelu.cz>
2025-10-23 23:08:41 +0200

    zmeny

--- src/components/GemCard.vue
+++ src/components/GemCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div
       class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition cursor-pointer"
+      @click="goToGemDetail"
   >
     <!-- Obrázek -->
     <div class="h-52 bg-gray-100">
@@ -13,22 +14,27 @@
       <p class="text-sm text-gray-500 line-clamp-2">{{ gem.description }}</p>
 
       <div class="mt-3 flex items-center justify-between">
-<!--        <span v-if="gem.price" class="text-emerald-700 font-bold">{{ gem.price }} €</span>-->
-        <router-link
-            :to="`/product/${gem.id}`"
-            class="text-sm px-3 py-1 rounded-full bg-emerald-600 text-white hover:bg-amber-500 transition"
-        >
+        <!-- Cenu můžeš zase zapnout -->
+        <!-- <span v-if="gem.price" class="text-emerald-700 font-bold">{{ gem.price }} €</span> -->
+        <span class="text-sm px-3 py-1 rounded-full bg-emerald-600 text-white hover:bg-amber-500 transition">
           View
-        </router-link>
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
 import type { Gem } from "../models/Gem";
 
-defineProps<{
+const props = defineProps<{
   gem: Gem;
 }>();
+
+const router = useRouter();
+
+const goToGemDetail = () => {
+  router.push(`/product/${props.gem.id}`);
+};
 </script>
